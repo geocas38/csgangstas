99cc437c003037b0c42de90cb5fcc3a80b186101
Chris from OSU <c.herczeg11@hotmail.com>
2016-07-25 15:06:20 -0700

    Added the fetch for attraction and resturant

--- touristhotspot/main.py
+++ touristhotspot/main.py
@@ -50,17 +50,46 @@ class ScheduleHandler(webapp2.RequestHandler):
             attractions = self.fetch_attractions(city, state, radius)
             resturants = self.fetch_resturants(city, state, radius)
             variables = {
-            
+                'search_attraction': attraction
+                'search_resturant': resturants
+
             }
+            self.response.write(template.render(variables))
+        else:
+            self.response.write("Please specify a city, state, or radius")
 
         ##Find attractions
     def fetch_attractions(self, city, state, radius):
 
-        ##Find Resturants
-    def fetch_resturants(self, city, state, radius ):
+        data_source = urlfetch(self.yelp_search(city, state, radius))
+        results = json.loads(data_source.content)
 
+        attractions = []
+        for attract_entry in results['data']
+            attractions.append(attract_entry['attractsite'])
+
+        return attractions
+        ##Find Resturants
+    def fetch_resturants(self, city, radius ):
+
+        data_source = urlfetch(self.yelp_search(city, state, radius))
+        results = json.loads(data_source.content)
+
+        resturants = []
+        for resturant_entry in results['data']
+            resturants.append(resturant_entry['attractsite'])
+
+        return attractions
         ##Utilize yelp search to find the resturants and attractions
-    def yelp_search(self, city, state, radius ):
+    def yelp_search(self, city, radius):
+
+        yelp_api = 'bM0VPHWh91R0g46amxYbnA'
+        base_url = 'https://api.yelp.com/v2/search?'
+        if
+        url_params= {
+            'c': city
+
+        }
 
 
 
