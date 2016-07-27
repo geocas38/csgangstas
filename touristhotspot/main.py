cd022231ee0d28eb0549b308e5ed3db2f0f362bb
Geovani the Terrible <geovanicas38@gmail.com>
2016-07-26 17:49:35 -0700

    Day 2 final version

--- touristhotspot/main.py
+++ touristhotspot/main.py
@@ -20,12 +20,9 @@ class MainHandler(webapp2.RequestHandler): #log-in page
             self.redirect('/intro')
         else:
             template = jinja_environment.get_template('tour.html')
-<<<<<<< HEAD
-        #    login = ('login':users.create_login_url('/'))
-=======
             login = {'login':users.create_login_url('/')}
->>>>>>> ed2c6e970d121c4510597a09f3cb59178b33064a
             self.response.out.write(template.render(login))
+            
 #This handler allows the user to chose if they want to "review" or if they want to make a schedule
 class IntroHandler(webapp2.RequestHandler):
     def get(self):
